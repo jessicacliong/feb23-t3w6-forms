c60c3b97a4a8ba1b794b25b1c9478c5ee5b7e42c
jessicacliong <14395@coderacademy.edu.au>
2023-09-21 20:01:58 +1000

    feat: components started

---
+++ src/BusinessCardParent.jsx
@@ -0,0 +1,22 @@
+import {Component} from "react";
+class BusinessCardParent extends Component {
+	constructor(){
+		super();
+	}
+}
+
+render(){
+  return(
+    <div>
+      <BusinessCardDisplay></BusinessCardDisplay>
+    </div>
+  )
+}
+}
+
+// import elephant from "react";
+// class BlahBlah extends elephant.Component {
+// 	constructor(){
+
+// 	}
+// }
